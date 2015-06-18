b828820320eee7759a16cf5f20d7f2b390b281c9
Matt Cheung <matt.cheung.dev@gmail.com>
2015-06-17 23:10:07 -0700

    adding location settings
    added location setting pref, no longer loading fake data

--- Sunshine/app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -1,9 +1,12 @@
 package com.example.android.sunshine.app;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -13,9 +16,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -57,8 +62,7 @@ public class ForecastFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_refresh){
-            FetchWeatherTask fetchWeather = new FetchWeatherTask();
-            fetchWeather.execute("94043");
+            updateWeather();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -67,16 +71,6 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        String[] forecastArray = {
-                "Today - Sunny - 88/63"
-                , "Tomorrow - Foggy - 70/46"
-                , "Weds - Cloudy - 72/63"
-                , "Thurs - Rainy - 64/51"
-                , "Fri - Foggy - 70/46"
-                , "Sat - Sunny - 76/68"
-        };
-
-        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
         mForecastAdapter = new ArrayAdapter<String>(
                 // The current context
                 getActivity()
@@ -85,14 +79,38 @@ public class ForecastFragment extends Fragment {
                 // ID of textview to populate
                 , R.id.list_item_forecast_textview
                 // Forecast data
-                , weekForecast);
+                , new ArrayList<String>());
 
         View rootView = inflater.inflate(R.layout.fragment_main, container);
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+                String forecast = mForecastAdapter.getItem(i);
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
+
+            }
+        });
         return rootView;
     }
 
+    private void updateWeather() {
+        FetchWeatherTask weatherTask = new FetchWeatherTask();
+        String location = PreferenceManager.getDefaultSharedPreferences(getActivity())
+                .getString(getString(R.string.pref_location_key)
+                        , getString(R.string.pref_location_default));
+        weatherTask.execute(location);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateWeather();
+    }
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
 
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
